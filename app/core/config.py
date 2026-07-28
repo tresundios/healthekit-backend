96c899e707346a843dba66547debe69eed1356c5
Navis Michael Bearly J <navis@tresundios.com>
2026-07-29 03:15:32 +0530

    Revert "update"
    This reverts commit 49707023bba64c0e5d038689474fd7b1715bc4c8.

--- app/core/config.py
+++ app/core/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
     ABDM_CLIENT_ID: str = "SBXID_043801"
     ABDM_CLIENT_SECRET: str = "CHANGE_ME"   # Jenkins credential / AWS SSM in real envs
     ABDM_GATEWAY_BASE: str = "https://dev.abdm.gov.in/api/hiecm"
-    ABHA_BASE: str = "https://abhasbx.abdm.gov.in/abha/api"
+    ABHA_BASE: str = "https://abhasbx.abdm.gov.in/abha/api/v3"
     ABDM_X_CM_ID: str = "sbx"
     ABDM_HIP_ID: str = "CHANGE_ME"          # your registered HIP service id
     ABDM_HIU_ID: str = "CHANGE_ME"
